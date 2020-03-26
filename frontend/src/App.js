a8cba127d5e3354d67ff06f35509c6f609becbfd
Lucas Xavier <lucasxavier92@gmail.com>
2020-03-26 00:00:01 -0300

    front-end do app web feito

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes';
+
+import './global.css';
+
+// JSX => Javascript XML
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <br>
-        </br>
-        <p>Olá mundo</p>
-        <p>Hello world</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes />
     </div>
   );
 }
